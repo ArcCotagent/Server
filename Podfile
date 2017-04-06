4c57d5d0d68ed98e0bf096365c2ad3d917e0f8aa
Arthur Wu (a.k.a Kuan-Ting Wu) <wu1106@purdue.edu>
2017-04-06 18:50:41 -0400

    retry

---
+++ Podfile
@@ -0,0 +1,19 @@
+source 'https://github.com/twilio/cocoapod-specs'
+
+workspace 'VideoQuickStart'
+
+abstract_target 'TwilioVideo' do
+  pod 'TwilioVideo', '1.0.0-beta12'
+
+  target 'VideoQuickStart' do
+    project 'VideoQuickStart.xcproject'
+  end
+  
+  target 'VideoCallKitQuickStart' do
+    project 'VideoCallKitQuickStart.xcproject'
+  end
+
+  target 'ScreenCapturerExample' do
+    project 'ScreenCapturerExample.xcproject'
+  end
+end
---
+++ ScreenCapturerExample/ViewController.swift
@@ -0,0 +1,136 @@
+//
+//  ViewController.swift
+//  ScreenCapturerExample
+//
+//  Copyright © 2016-2017 Twilio, Inc. All rights reserved.
+//
+
+import TwilioVideo
+import UIKit
+import WebKit
+
+class ViewController : UIViewController {
+
+    var localMedia: TVILocalMedia?
+    var remoteRenderer: TVIVideoViewRenderer?
+    var screenCapturer: TVIVideoCapturer?
+    var webView: WKWebView?
+    var webNavigation: WKNavigation?
+
+    // Set this value to 'true' to use ExampleScreenCapturer instead of TVIScreenCapturer.
+    let useExampleCapturer = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Setup a WKWebView, and request Twilio's website
+        webView = WKWebView.init(frame: view.frame)
+        webView?.navigationDelegate = self
+        webView?.translatesAutoresizingMaskIntoConstraints = false
+        webView?.allowsBackForwardNavigationGestures = true
+        self.view.addSubview(webView!)
+
+        let requestURL: URL = URL(string: "https://twilio.com")!
+        let request = URLRequest.init(url: requestURL)
+        webNavigation = webView?.load(request)
+
+        setupLocalMedia()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        webView?.frame = self.view.bounds
+
+        // Layout the remote video using frame based techniques. It's also possible to do this using autolayout
+        if ((remoteRenderer?.hasVideoData)!) {
+            let dimensions = remoteRenderer?.videoFrameDimensions
+            let remoteRect = remoteViewSize()
+            let aspect = CGSize(width: CGFloat((dimensions?.width)!), height: CGFloat((dimensions?.height)!))
+            let padding : CGFloat = 10.0
+            let boundedRect = AVMakeRect(aspectRatio: aspect, insideRect: remoteRect).integral
+            remoteRenderer?.view.frame = CGRect(x: self.view.bounds.width - boundedRect.width - padding,
+                                                y: self.view.bounds.height - boundedRect.height - padding,
+                                                width: boundedRect.width,
+                                                height: boundedRect.height)
+        } else {
+            remoteRenderer?.view.frame = CGRect.zero
+        }
+    }
+
+    func setupLocalMedia() {
+        localMedia = TVILocalMedia()
+
+        // Setup screen capturer
+        let capturer: TVIVideoCapturer
+        if (useExampleCapturer) {
+            capturer = ExampleScreenCapturer.init(aView: self.webView!)
+        } else {
+            capturer = TVIScreenCapturer.init(view: self.webView!)
+        }
+
+        let videoTrack: TVIVideoTrack? = localMedia?.addVideoTrack(true, capturer: capturer)
+
+        if (videoTrack == nil) {
+            presentError(message: "Failed to add screen capturer track!")
+            return;
+        }
+
+        screenCapturer = capturer;
+
+        // Setup rendering
+        remoteRenderer = TVIVideoViewRenderer(delegate: self)
+        videoTrack?.addRenderer(remoteRenderer!)
+
+        remoteRenderer?.view.isHidden = true
+        self.view.addSubview((remoteRenderer?.view)!)
+        self.view.setNeedsLayout()
+    }
+
+    func presentError( message: String) {
+        print(message)
+    }
+
+    func remoteViewSize() -> CGRect {
+        let traits = self.traitCollection
+        let width = traits.horizontalSizeClass == UIUserInterfaceSizeClass.regular ? 188 : 160;
+        let height = traits.horizontalSizeClass == UIUserInterfaceSizeClass.regular ? 188 : 120;
+        return CGRect(x: 0, y: 0, width: width, height: height)
+    }
+}
+
+// MARK: WKNavigationDelegate
+extension ViewController : WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("WebView:", webView, "finished navigation:", navigation)
+
+        self.navigationItem.title = webView.title
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        let message = String(format: "WebView:", webView, "did fail navigation:", navigation, error as CVarArg)
+        presentError(message: message)
+    }
+}
+
+// MARK: TVIVideoRendererDelegate
+extension ViewController : TVIVideoViewRendererDelegate {
+    func rendererDidReceiveVideoData(_ renderer: TVIVideoViewRenderer) {
+        if (renderer == remoteRenderer) {
+            remoteRenderer?.view.isHidden = false
+            self.view.setNeedsLayout()
+        }
+    }
+
+    func renderer(_ renderer: TVIVideoViewRenderer, dimensionsDidChange dimensions: CMVideoDimensions) {
+        self.view.setNeedsLayout()
+    }
+}
